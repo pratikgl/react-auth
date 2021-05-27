6d4c79c11f89bc05987c0d760e6a1782c7cea956
pratikgl <pratikgoyal2712@gmail.com>
2021-05-27 14:37:41 +0530

    Sign up page front end

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
+import React from 'react'
+import SignUp from "./SignUp";
+import { Container } from "react-bootstrap";
+
 function App() {
   return (
-    "Hello world"
+    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+      <div className="w-100" style={{ maxWidth: "400px" }}><SignUp /></div>
+    </Container>
   );
 }
 
