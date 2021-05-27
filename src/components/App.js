75f92714b5cfe77b69bf83cf71b0da0bbf182042
pratikgl <pratikgoyal2712@gmail.com>
2021-05-27 23:50:46 +0530

    login route added and connected few routes

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import { Container } from "react-bootstrap";
 import { AuthProvider } from '../contexts/AuthContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Dashboard from './Dashboard'
+import Login from './Login'
+
 function App() {
   return (
     <AuthProvider>
@@ -12,8 +14,9 @@ function App() {
           <Router>
             <AuthProvider>
               <Switch>
-              <Route exact path='/' component={Dashboard}/>
-                <Route path='/signup' component={SignUp}/>
+                <Route exact path='/' component={Dashboard} />
+                <Route path='/signup' component={SignUp} />
+                <Route path='/login' component={Login} />
               </Switch>
             </AuthProvider>
           </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,19 +3,15 @@ import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 import { Link } from 'react-router-dom'
 
-export default function SignUp() {
+export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
   const { signup } = useAuth();
   const [error, setError] = useState('')
   const [loading, setLoading] = useState(false)
 
   async function handleSubmit(e) {
     e.preventDefault()
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError('Passwords do not match')
-    }
     try {
       setError('')
       setLoading(true)
@@ -29,7 +25,7 @@ export default function SignUp() {
   return (
     <>
       <Card body>
-        <h2 className="text-center mb-4">Sign Up</h2>
+        <h2 className="text-center mb-4">Log In</h2>
         {error && <Alert variant='danger'>{error}</Alert>}
         <Form onSubmit={handleSubmit}>
           <Form.Group id='email'>
@@ -42,15 +38,12 @@ export default function SignUp() {
             <Form.Control type='password' ref={passwordRef} required placeholder='Enter password'></Form.Control>
           </Form.Group>
 
-          <Form.Group id='password-confirm'>
-            <Form.Label>Password Confirmation</Form.Label>
-            <Form.Control type='password' ref={passwordConfirmRef} required placeholder='Confirm password'></Form.Control>
-          </Form.Group>
-
-          <Button disabled={loading} type="submit" className="w-100">Sign Up</Button>
+          <Button disabled={loading} type="submit" className="w-100">
+            Log In
+          </Button>
         </Form>
       </Card>
-      <div className="w-100 text-center mt-2">Already have an account? <Link to='login'>Log In</Link> </div>
+      <div className="w-100 text-center mt-2">Need an account? <Link to='/signup'>Sign Up</Link></div>
     </>
   )
 }
